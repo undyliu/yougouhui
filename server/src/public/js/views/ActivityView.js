d8b5e5f70902867fd60a0262d720648eae55bdea
liuxiaoyong <undyliu@126.com>
2014-03-04 17:29:25 +0800

    产品详细页面img图片增加baseUrl

--- server/src/public/js/views/ActivityView.js
+++ server/src/public/js/views/ActivityView.js
@@ -1,6 +1,6 @@
 
-define([ "jquery", "backbone", "views/BackModelView", "utils" ]
-	, function( $, Backbone, CallbackView, Utils ) {
+define([ "jquery", "backbone", "views/BackModelView", "utils", "config" ]
+	, function( $, Backbone, CallbackView, Utils, appConf ) {
 
     var View = CallbackView.extend( {
     		constructor:function(options) {
@@ -16,7 +16,8 @@ define([ "jquery", "backbone", "views/BackModelView", "utils" ]
           Utils.setDomVisibleExcept($('[data-role="header"].app-header div a'), []);
           
           //商品折扣基本信息
-          this.template = _.template( $( "script#activityItem" ).html(), { "activity": this.model } );            
+          this.template = _.template( $( "script#activityItem" ).html(), 
+						{ "activity": this.model, "baseUrl": appConf.getBaseUrl() } );            
           this.$el.find('[role="main"] #activity-content').html(this.template);
           
           //点评信息
